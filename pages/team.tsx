4076831fefbed6ec14f4c8c5e490a2625d99abb1
Luc Succes <l2succes@gmail.com>
2022-03-22 17:00:49 -0400

    Adds images

--- pages/team.tsx
+++ pages/team.tsx
@@ -4,13 +4,15 @@ import { Layout } from "../components/Layout"
 import { Nav } from "../components/Nav"
 
 const Team = () => {
-  const Person = ({ name, bio }: { name: string; bio: string }) => {
+  const Person = ({ name, bio, image }: { name: string; bio: string; image: string }) => {
     return (
       <div className="my-5 mb-20 flex">
         <div className="">
-          <div className="relative h-[92px] w-[92px] overflow-hidden rounded-full border-2 border-indigo-600">
-            <img src="/images/profile-image.jpg" className="absolute inset-0" />
-          </div>
+          <div
+            // @ts-ignore
+            style={{ backgroundImage: `url(${image})` }}
+            className={`relative h-[92px] w-[92px] overflow-hidden rounded-full border-2 border-indigo-600 bg-cover bg-center`}
+          ></div>
         </div>
         <div className="pl-10">
           <div className="mb-4 text-xl">{name}</div>
@@ -26,42 +28,49 @@ const Team = () => {
       name: "Dervin E. Dimanche",
       title: "",
       bio: "",
+      image: "",
     },
     {
       id: "2",
       name: "Isaac Udogwu",
       title: "Artist & Co-Founder",
       bio: "A Nigerian-American digital Artist & Martial Artist that creates worlds that allow people of the African Diaspora to be fully liberated and just simply exist as humans. Using afro-futurism as a means to create these worlds because, as a friend once told him, “Afro-Futurism is simply black people existing in the future”. ",
+      image: "",
     },
     {
       id: "3",
       name: "Jesse Aridoux",
       title: "Design Lead",
       bio: "A seasoned visual designer with over a decade of experience, and a diverse background in the design field. He has built many brands and worked with companies in various industries from fashion/lifestyle, cosmetics, tech and more. He also leverages his craft as a fine artist to influence his design work.",
+      image: "",
     },
     {
       id: "4",
       name: "Luc Sucess",
       title: "Technical Lead",
       bio: "A 2x Start up Founder, Web and Mobile Engineer with 10+ Years of Development experience. He has worked namely at Spotify on discover weekly, and at Artsy as a Tech Lead. Luc has a passion for music production and Traveling.",
+      image: "/images/luc-succes.jpg",
     },
     {
       id: "5",
       name: "Corey Neufville",
       title: "Program & Partnerships Lead",
       bio: "Multi Disciplined background in the tech space for over 13 years. Has experience in leading strategic initiatives from brand building to product development.  Spent 12+ years at Meta helping external partners & businesses fulfilling their business objectives on the platform.",
+      image: "/images/corey-neufville.png",
     },
     {
       id: "6",
       name: "Pedro Acosta",
       title: "Sr. Software Engineer",
       bio: "Passionate about the future of the world and what innovative Tech can bring, Pedro pivoted careers transitioning from Law Enforcement to Tech. Building his expertise at an AI company, and founding TrueBonds - a platform that enables planning Funeral Services online. Pedro is also in addition to Engineering an avid Real Estate & Crypto investor by day- while holding top 75 ranking in NYC on Super Smash Brothers Ultimate by night. (Greninja main)",
+      image: "/images/pedro-acosta.png",
     },
     {
       id: "7",
       name: "Rashawn Evans",
       title: "Sr. Software Engineer",
       bio: "User Centered Product Strategist and Gifted Front-End Developer with a passion for building and designing technology of the future. Rashawn is also founder of Start-up ‘PledgeNexus’ an Video Streaming Platform for Poets, Musicians, & Comedians giving young amateur artists a platform to grow, collaborate, and gain exposure. ",
+      image: "/images/rashawn-evans.png",
     },
   ]
 
