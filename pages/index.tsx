4db04eddcadda27ef7092d535034c935b6840eaa
Luc Succes <l2succes@gmail.com>
2022-03-22 13:58:57 -0400

    Font size

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next"
 import Head from "next/head"
-import { Center, Box, Flex } from "@chakra-ui/react"
+import { Center, Box, Flex, Text } from "@chakra-ui/react"
 import { DiasporaLogo } from "../components/DiasporaLogo"
 
 const Home: NextPage = () => {
@@ -12,10 +12,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Flex></Flex>
+
       <Center bg="black" color="white" height="100vh">
         <Flex padding="4" direction="column" align="center" justify="center">
           <DiasporaLogo />
-          <Box m={4}>Coming soon</Box>
+          <Box m={4}>
+            <Text fontSize="large">Coming soon</Text>
+          </Box>
         </Flex>
       </Center>
 
