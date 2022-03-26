503d231e7c2b5a454377a002fccd710728ae3661
Luc Succes <l2succes@gmail.com>
2022-03-26 13:25:00 -0400

    Fixes nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,18 +6,18 @@ export const Nav = () => {
   const router = useRouter()
 
   return (
-    <div className="flex w-full flex-row text-right md:flex-col">
-      <div className="bg-black p-2">
+    <div className="flex w-full flex-row rounded-md  px-3 text-right md:flex-col">
+      <div className=" bg-black p-4">
         <Link href="/">
           <a className={router.pathname == "/" ? "text-indigo-600" : ""}>Home</a>
         </Link>
       </div>
-      <div className="bg-black p-2">
+      <div className=" bg-black p-4">
         <Link href="/team">
           <a className={router.pathname == "/team" ? "text-indigo-600" : ""}>Team</a>
         </Link>
       </div>
-      <div className="bg-black p-2">
+      <div className="bg-black p-4">
         <Link href="/whitepaper">
           <a className={router.pathname == "/whitepaper" ? "text-indigo-600" : ""}>White Paper</a>
         </Link>
