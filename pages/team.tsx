bdc7d9fb10f4352160778c1dcecfb1d72c0c1e69
Pedro Acosta <pedro.acosta613@gmail.com>
2022-03-27 12:44:36 -0400

    added cyber

--- pages/team.tsx
+++ pages/team.tsx
@@ -13,6 +13,7 @@ const Team = () => {
     twitter,
     instagram,
     dribbble,
+    cyber,
   }: {
     name: string
     bio: string
@@ -22,6 +23,7 @@ const Team = () => {
     twitter?: string
     instagram?: string
     dribbble?: string
+    cyber?: string
   }) => {
     return (
       <div className="my-5 mb-20 flex flex-col items-center md:flex-row md:items-start">
@@ -60,6 +62,13 @@ const Team = () => {
                 <div className="mr-2">
                   <a target="_blank" rel="noreferrer" href={dribbble}>
                     <social.dribbble.icon width="24" height="24" background="#fff" />
+                  </a>
+                </div>
+              )}
+              {cyber && (
+                <div className="mr-2">
+                  <a target="_blank" rel="noreferrer" href={cyber}>
+                    <social.cyber.icon width="24" height="24" background="#fff" />
                   </a>
                 </div>
               )}
@@ -125,6 +134,15 @@ const Team = () => {
         </svg>
       ),
     },
+    cyber: {
+      name: "Cyber",
+      icon: (props: any) => (
+
+        <svg fill="currentColor" viewBox="0 0 30 30" {...props}>
+          <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z" />
+        </svg>       
+      ),
+    },
   }
 
   const people = [
@@ -135,6 +153,7 @@ const Team = () => {
       bio: "Born & raised Haitian American, Dervin is a Technologist and Networker with extensive experience building high performance teams, advising Start-Ups, Product Development & leveraging Technology trends to Seize Market Opportunities - namely coming from Google & Meta. Dervin has an insatiable thirst for leveraging technology to push the global Diaspora forward and has made it a LifeLong Quest. He is passionate about History, Traveling, Design, Fashion, Philanthropy and the synergy of them all with Tech.",
       image: "/images/dervin-dimanche.png",
       linkedIn: "https://www.linkedin.com/in/dervindimanche/",
+      cyber: "https://www.cyber.xyz/w0rldpr0test",
     },
     {
       id: "2",
