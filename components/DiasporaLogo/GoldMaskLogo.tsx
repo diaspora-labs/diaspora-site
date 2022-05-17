13ef6dd2e15a92ed6dfa70c03f8a13fa5095933d
Luc Succes <l2succes@gmail.com>
2022-05-17 01:29:19 -0500

    Gold mask logo

--- components/DiasporaLogo/GoldMaskLogo.tsx
+++ components/DiasporaLogo/GoldMaskLogo.tsx
@@ -3,6 +3,6 @@ const Spline = dynamic(() => import("@splinetool/react-spline"), {
   ssr: false,
 })
 
-export default () => {
+export default function GoldMaskLogo() {
   return <Spline scene="https://prod.spline.design/9I5EiVB3Oko8EpdM/scene.splinecode" className="absolute inset-0" />
 }
