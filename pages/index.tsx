e9738527318dcafbe61d02ccfa37a56efac9721a
pedro.acosta <pedro.acosta613@gmail.com>
2022-05-27 15:40:11 -0400

    removed dead code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense, useEffect } from "react"
 import { NextPage } from "next"
-import Image from 'next/image'
 import dynamic from "next/dynamic"
 // import GoldMaskLogo from "../components/DiasporaLogo/GoldMaskLogo"
 import { HomeFooter } from "../components/Home/HomeFooter"
@@ -10,6 +9,7 @@ import { Instagram } from "../components/Icons/Instagram"
 import { Twitter } from "../components/Icons/Twitter"
 import { Layout } from "../components/Layout"
 import { NavHeader } from "../components/NavHeader"
+import Team from './team'
 import sal from "sal.js"
 
 const GoldMaskLogo = dynamic(() => import("../components/DiasporaLogo/GoldMaskLogo"), {
@@ -93,46 +93,6 @@ const Home: NextPage = () => {
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
             </p>
           </div>
-          {/*<div className="grid h-full grow grid-cols-4">
-
-            <div className="col-span-2 flex h-full flex-col items-start justify-center">
-              <div
-                className="text-5xl font-light leading-tight text-neutral-600"
-                data-sal="slide-up"
-                data-sal-delay="300"
-              >
-                Building a bridge
-                <br />
-                for the culture
-                <br />& mint an ancestor
-              </div>
-            </div>
-            <div
-              className="col-span-2 mx-20 flex h-full flex-col items-end justify-center"
-              data-sal="slide-up"
-              data-sal-delay="500"
-            >
-              <div className="flex flex-col items-center justify-center space-y-6">
-                <div className="mt-[80px] text-center text-4xl font-light">
-                  Great things
-                  <br />
-                  are coming
-                </div>
-                <div className="text-lg font-normal">Connect with us to stay updated</div>
-                <div className="pointer-events-auto flex space-x-10">
-                  <Discord />
-                  <Instagram />
-                  <Twitter />
-                  <Envelop />
-                </div>
-                <div className="hidden p-2">
-                  <button className="rounded-xl bg-[#F05E17] px-4 py-2 font-bold uppercase text-black">
-                    Connect Wallet
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>*/}
 
         </div>
 
