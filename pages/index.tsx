240336909db6e576bb7263f3d1ca873e82ca8e08
pedro.acosta <pedro.acosta613@gmail.com>
2022-05-27 15:33:44 -0400

    made container longer

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,7 +88,7 @@ const Home: NextPage = () => {
               height="172" />
           </div>
 
-          <div className={`container mx-auto mt-10 max-w-2xl`}>
+          <div className={`container mx-auto mt-10 max-w-3xl`}>
             <p className={`text-center tracking-wide text-lg font-normal text-gray-400 `}>
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
             </p>
