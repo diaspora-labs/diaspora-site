0aa8f5b6b09990859697b2b5ceac6e8656fdabf9
pedro.acosta <pedro.acosta613@gmail.com>
2022-05-27 08:33:19 -0400

    created a new section for the team in home page. added new logo for temp use. created folder for images to help with organization.

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
       <div className="fixed inset-0 z-0">
         {/* @ts-ignore */}
         <Suspense fallback={null} r3f>
-          <GoldMaskLogo />
+          {/*<GoldMaskLogo />*/}
         </Suspense>
       </div>
 
@@ -36,6 +36,50 @@ const Home: NextPage = () => {
         <div className="flex grow flex-col p-10">
           <NavHeader />
 
+          <div className="grid h-full grow grid-cols-4">
+            <div className="col-span-2 flex h-full flex-col items-start justify-center">
+              <div
+                className="text-5xl font-light leading-tight text-neutral-600"
+                data-sal="slide-up"
+                data-sal-delay="300"
+              >
+                Building a bridge
+                <br />
+                for the culture
+                <br />& mint an ancestor
+              </div>
+            </div>
+            <div
+              className="col-span-2 mx-20 flex h-full flex-col items-end justify-center"
+              data-sal="slide-up"
+              data-sal-delay="500"
+            >
+              <div className="flex flex-col items-center justify-center space-y-6">
+                <div className="mt-[80px] text-center text-4xl font-light">
+                  Great things
+                  <br />
+                  are coming
+                </div>
+                <div className="text-lg font-normal">Connect with us to stay updated</div>
+                <div className="pointer-events-auto flex space-x-10">
+                  <Discord />
+                  <Instagram />
+                  <Twitter />
+                  <Envelop />
+                </div>
+                <div className="hidden p-2">
+                  <button className="rounded-xl bg-[#F05E17] px-4 py-2 font-bold uppercase text-black">
+                    Connect Wallet
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pointer-events-none z-10 flex h-full min-h-screen flex-col border-t-[1px] border-neutral-800">
+        <div className="flex grow flex-col p-10">
           <div className="grid h-full grow grid-cols-4">
             <div className="col-span-2 flex h-full flex-col items-start justify-center">
               <div
