bbd5f1e013ebb93e49b733f14ea6a3a5ffc5f969
pedro.acosta <pedro.acosta613@gmail.com>
2022-05-31 07:41:01 -0400

    fix bug and worked on cols

--- components/Icons/Profile.tsx
+++ components/Icons/Profile.tsx
@@ -6,8 +6,8 @@ export function Profile(props: { color?: string }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="25.044" height="25.044" viewBox="0 0 25.044 25.044">
       <g id="profile-ope-icon" transform="translate(0.5 0.5)">
-        <line id="Line_7" data-name="Line 7" y2="24.044" transform="translate(12.022)" fill="none" stroke="#2d2a26" stroke-linecap="round" stroke-width="1"/>
-        <line id="Line_8" data-name="Line 8" y2="24.044" transform="translate(24.044 12.022) rotate(90)" fill="none" stroke="#2d2a26" stroke-linecap="round" stroke-width="1"/>
+        <line id="Line_7" data-name="Line 7" y2="24.044" transform="translate(12.022)" fill="none" stroke="#2d2a26" strokeLinecap="round" strokeWidth="1"/>
+        <line id="Line_8" data-name="Line 8" y2="24.044" transform="translate(24.044 12.022) rotate(90)" fill="none" stroke="#2d2a26" strokeLinecap="round" strokeWidth="1"/>
       </g>
     </svg>
 
