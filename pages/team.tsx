a9b8a62b3c357fa29a68f59c4f18b25a9619324a
pedro.acosta <pedro.acosta613@gmail.com>
2022-06-05 13:28:12 -0400

    added modal from tailwind. testing

--- pages/team.tsx
+++ pages/team.tsx
@@ -31,87 +31,113 @@ const Team = () => {
     streetArt?: string,
   }) => {
     return (
-      <div className="lg:ml-10 mb-5 border-2 rounded-[40px] border-neutral-800">
-        
-        <div class="relative">
-          <div class="absolute top-10 right-7">
-            <Profile  />
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center lg:flex-row">
-
-          <div className="mb-5 mt-5 lg:ml-7">
-            <div
-              style={{ backgroundImage: `url(${image})` }}
-              className={`relative h-[100px] w-[100px] overflow-hidden rounded-full border-2 border-neutral-800 bg-cover bg-top`}
-            ></div>
-          </div>
-          <div className="px-5 text-left">
-            <div className="flex items-center">
-              <div className="mb-1 text-xl">{name}</div>
-              {/*<div className="ml-3 mt-[-3px] flex">
-                {linkedIn && (
-                  <div className="mr-2">
-                    <a target="_blank" rel="noreferrer" href={linkedIn}>
-                      <social.linkedIn.icon width="24" height="24" background="#fff" />
-                    </a>
-                  </div>
-                )}
-                {twitter && (
-                  <div className="mr-2">
-                    <a target="_blank" rel="noreferrer" href={twitter}>
-                      <social.twitter.icon width="24" height="24" background="#fff" />
-                    </a>
-                  </div>
-                )}
-                {instagram && (
-                  <div className="mr-2">
-                    <a target="_blank" rel="noreferrer" href={instagram}>
-                      <social.instagram.icon width="24" height="24" background="#fff" />
-                    </a>
-                  </div>
-                )}
-                {dribbble && (
-                  <div className="mr-2">
-                    <a target="_blank" rel="noreferrer" href={dribbble}>
-                      <social.dribbble.icon width="24" height="24" background="#fff" />
-                    </a>
-                  </div>
-                )}
-                {cyber && (
-                  <div className="mr-2">
-                    <a target="_blank" rel="noreferrer" href={cyber} className="mt-1 block">
-                      <social.cyber.icon width="24" background="#fff" />
-                    </a>
-                  </div>
-                )}
-                {foundation && (
-                  <div className="mr-2">
-                    <a target="_blank" rel="noreferrer" href={foundation} className="mt-1 block">
-                      <social.foundation.icon width="24" background="#fff" />
-                    </a>
-                  </div>
-                )}
-                {streetArt && (
-                  <div className="mr-2">
-                    <a target="_blank" rel="noreferrer" href={streetArt} className="mt-1 block">
-                      <social.streetArt.icon width="24" background="#fff" />
-                    </a>
-                  </div>
-                )}
-              </div>*/}
-            </div>
-            <div className="text-md mb-4 text-gray-400 text-center">{title}</div>
-            {/*<div className="text-neutral-500">{bio}</div>*/}
-          </div>
-
+      <div>
+        <div data-modal-toggle="defaultModal" className="lg:ml-10 mb-5 border-2 rounded-[40px] border-neutral-800">
           
+          <div className="relative">
+            <div className="absolute top-10 right-7">
+              <Profile  />
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center lg:flex-row">
+
+            <div className="mb-5 mt-5 lg:ml-7">
+              <div
+                style={{ backgroundImage: `url(${image})` }}
+                className={`relative h-[100px] w-[100px] overflow-hidden rounded-full border-2 border-neutral-800 bg-cover bg-top`}
+              ></div>
+            </div>
+            <div className="px-5 text-left">
+              <div className="flex items-center">
+                <div className="mb-1 text-xl">{name}</div>
+                {/*<div className="ml-3 mt-[-3px] flex">
+                  {linkedIn && (
+                    <div className="mr-2">
+                      <a target="_blank" rel="noreferrer" href={linkedIn}>
+                        <social.linkedIn.icon width="24" height="24" background="#fff" />
+                      </a>
+                    </div>
+                  )}
+                  {twitter && (
+                    <div className="mr-2">
+                      <a target="_blank" rel="noreferrer" href={twitter}>
+                        <social.twitter.icon width="24" height="24" background="#fff" />
+                      </a>
+                    </div>
+                  )}
+                  {instagram && (
+                    <div className="mr-2">
+                      <a target="_blank" rel="noreferrer" href={instagram}>
+                        <social.instagram.icon width="24" height="24" background="#fff" />
+                      </a>
+                    </div>
+                  )}
+                  {dribbble && (
+                    <div className="mr-2">
+                      <a target="_blank" rel="noreferrer" href={dribbble}>
+                        <social.dribbble.icon width="24" height="24" background="#fff" />
+                      </a>
+                    </div>
+                  )}
+                  {cyber && (
+                    <div className="mr-2">
+                      <a target="_blank" rel="noreferrer" href={cyber} className="mt-1 block">
+                        <social.cyber.icon width="24" background="#fff" />
+                      </a>
+                    </div>
+                  )}
+                  {foundation && (
+                    <div className="mr-2">
+                      <a target="_blank" rel="noreferrer" href={foundation} className="mt-1 block">
+                        <social.foundation.icon width="24" background="#fff" />
+                      </a>
+                    </div>
+                  )}
+                  {streetArt && (
+                    <div className="mr-2">
+                      <a target="_blank" rel="noreferrer" href={streetArt} className="mt-1 block">
+                        <social.streetArt.icon width="24" background="#fff" />
+                      </a>
+                    </div>
+                  )}
+                </div>*/}
+              </div>
+              <div className="text-md mb-4 text-gray-400 text-center">{title}</div>
+              {/*<div className="text-neutral-500">{bio}</div>*/}
+            </div>
+
+          </div>
 
         </div>
 
+        <div id="defaultModal"  aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+          <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
+              <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+                  <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                      <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                          Terms of Service
+                      </h3>
+                      <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>  
+                      </button>
+                  </div>
+
+                  <div className="p-6 space-y-6">
+                      <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                          With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                      </p>
+                      <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                          The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                      </p>
+                  </div>
+              </div>
+          </div>
+        </div>
 
       </div>
+        
     )
   }
 
@@ -277,6 +303,7 @@ const Team = () => {
           ))}
         </div>
       </div>
+
     </Layout>
   )
 }
