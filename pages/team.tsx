92cbc4e8b61136c884c05e471fb6f80d9d3b6875
pedro.acosta <pedro.acosta613@gmail.com>
2022-06-09 15:51:13 -0400

    added svg for exting modal

--- pages/team.tsx
+++ pages/team.tsx
@@ -286,7 +286,9 @@ const Team = () => {
 
                   <div className="relative">
                     <div className="absolute top-10 right-7">
-                      <Profile  />
+                      <button type="button" class="text-black bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                        <svg class="w-10 h-10" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+                      </button>
                     </div>
                   </div>
 
@@ -299,60 +301,7 @@ const Team = () => {
                       ></div>
                     </div>
                     <div className="px-5 text-left py-20 pr-20">
-                      <div className="flex items-center">
-                        
-                        {/*<div className="ml-3 mt-[-3px] flex">
-                          {linkedIn && (
-                            <div className="mr-2">
-                              <a target="_blank" rel="noreferrer" href={linkedIn}>
-                                <social.linkedIn.icon width="24" height="24" background="#fff" />
-                              </a>
-                            </div>
-                          )}
-                          {twitter && (
-                            <div className="mr-2">
-                              <a target="_blank" rel="noreferrer" href={twitter}>
-                                <social.twitter.icon width="24" height="24" background="#fff" />
-                              </a>
-                            </div>
-                          )}
-                          {instagram && (
-                            <div className="mr-2">
-                              <a target="_blank" rel="noreferrer" href={instagram}>
-                                <social.instagram.icon width="24" height="24" background="#fff" />
-                              </a>
-                            </div>
-                          )}
-                          {dribbble && (
-                            <div className="mr-2">
-                              <a target="_blank" rel="noreferrer" href={dribbble}>
-                                <social.dribbble.icon width="24" height="24" background="#fff" />
-                              </a>
-                            </div>
-                          )}
-                          {cyber && (
-                            <div className="mr-2">
-                              <a target="_blank" rel="noreferrer" href={cyber} className="mt-1 block">
-                                <social.cyber.icon width="24" background="#fff" />
-                              </a>
-                            </div>
-                          )}
-                          {foundation && (
-                            <div className="mr-2">
-                              <a target="_blank" rel="noreferrer" href={foundation} className="mt-1 block">
-                                <social.foundation.icon width="24" background="#fff" />
-                              </a>
-                            </div>
-                          )}
-                          {streetArt && (
-                            <div className="mr-2">
-                              <a target="_blank" rel="noreferrer" href={streetArt} className="mt-1 block">
-                                <social.streetArt.icon width="24" background="#fff" />
-                              </a>
-                            </div>
-                          )}
-                        </div>*/}
-                      </div>
+                      
                       <div className="mb-1 text-2xl text-black">{showPerson.name}</div>
                       <div className="text-lg mb-4 text-black">{showPerson.title}</div>
                       <div className="text-sm text-black">{showPerson.bio}</div>
