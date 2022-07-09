00ec15ad4c9cc3ed991f84f21052a123eda3a1f9
Rashawn Evans <Reva8390@gmail.com>
2022-07-09 09:51:32 -0400

    updating footer styles

--- components/Home/HomeFooter.tsx
+++ components/Home/HomeFooter.tsx
@@ -5,8 +5,8 @@ import { Twitter } from "../Icons/Twitter"
 
 export const HomeFooter = () => {
   return (
-    <footer className="flex-col rounded-lg p-4 shadow dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6">
-      <div className="w-full flex-row flex-wrap justify-between border-t-[1px] border-neutral-800 py-10 md:flex md:justify-between md:px-10">
+    <footer className="flex-col rounded-lg p-4 py-10 shadow dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6 md:px-10">
+      <div className="w-full flex-row flex-wrap justify-between border-t-[1px] border-neutral-800 md:flex md:justify-between">
         <div className="md:flex">
           <div className="mb-5 shrink rounded-2xl bg-neutral-800 py-2 px-4 text-center md:rounded-r-none">
             <input placeholder="Email address" className=" bg-transparent text-center md:text-left" />
@@ -16,7 +16,7 @@ export const HomeFooter = () => {
           </div>
         </div>
 
-        <div className="mt-10 items-center md:mt-0 md:flex">
+        <div className="mt-10 items-center space-x-10 md:mt-0 md:flex">
           <div className="mb-5 flex flex-row items-center justify-center space-x-10 text-sm font-light text-neutral-500">
             <div className=" ">
               <a href="/contact-us">Contact us</a>
@@ -25,8 +25,8 @@ export const HomeFooter = () => {
               <a href="/terms">Terms and conditions</a>
             </div>
           </div>
-          <div className="mx-4 mb-10 md:flex">
-            <span className="flex flex-row  justify-center space-x-12">
+          <div className="mb-5 md:flex">
+            <span className="flex flex-row justify-center space-x-12">
               <Discord color="rgba(255,255,255,0.5)" />
               <Instagram color="rgba(255,255,255,0.5)" />
               <Twitter color="rgba(255,255,255,0.5)" />
@@ -35,10 +35,8 @@ export const HomeFooter = () => {
         </div>
       </div>
 
-      <div className="md:flex ">
-        <div className="mb-5 flex flex-row items-center justify-center space-x-10 text-xs font-medium text-neutral-500	">
-          &copy; 2022 DIASPORA
-        </div>
+      <div className="mb-5 flex w-full flex-row justify-center space-x-10 text-xs font-medium text-neutral-500 md:flex md:w-full md:justify-start	">
+        &copy; 2022 DIASPORA
       </div>
     </footer>
   )
