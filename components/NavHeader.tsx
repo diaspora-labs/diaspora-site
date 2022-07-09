bbd4ae17d9887e4464121921436ed9ae4d51b123
Rashawn Evans <Reva8390@gmail.com>
2022-07-09 09:27:12 -0400

    fix nav on mobile

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { DiasporaLogoSmall } from "../components/DiasporaLogoSmall"
 import Link from "next/link"
 import { Nav } from "./Nav"
@@ -7,6 +7,11 @@ import { Instagram } from "./Icons/Instagram"
 import { Twitter } from "./Icons/Twitter"
 
 export const NavHeader = () => {
+  const [width, setWidth] = useState(0)
+
+  useEffect(() => {
+    setWidth(window.innerWidth)
+  }, [])
   return (
     <nav className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -21,7 +26,7 @@ export const NavHeader = () => {
         <div className="grow"></div>
 
         <div className="mx-4 flex items-center">
-          <Nav />
+          {width > 800 && <Nav />}
           <span className="ml-10 flex flex-row items-center space-x-12">
             <Discord color="rgba(255,255,255,0.5)" />
             <Instagram color="rgba(255,255,255,0.5)" />
@@ -29,6 +34,11 @@ export const NavHeader = () => {
           </span>
         </div>
       </div>
+      {width <= 800 && (
+        <div className="flex justify-center">
+          <Nav />
+        </div>
+      )}
     </nav>
   )
 }
