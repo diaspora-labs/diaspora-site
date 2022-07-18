a8d14e5c36fdb7c7305be300a2864b37d1706612
Luc Succes <l2succes@gmail.com>
2022-07-18 13:51:16 +0200

    Navigation bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,28 +1,29 @@
 import React from "react"
 import Link from "next/link"
 import { useRouter } from "next/router"
+import cls from "classnames"
 
 export const Nav = () => {
   const router = useRouter()
 
+  const baseClasses = "transition ease-in-out hover:text-indigo-600"
+  const routes = {
+    // "/gallery": "gallery",
+    "/roadmap": "roadmap",
+    "/whitepaper": "about",
+  }
+
   return (
     <div className="flex items-center space-x-1">
-      {/* TODO: uncomment after launching gallery */}
-      {/* <div className="p-4">
-        <Link href="/gallery">
-          <a className={router.pathname == "/team" ? "koro text-indigo-600" : ""}>GALLERY</a>
-        </Link>
-      </div> */}
-      <div className="p-4">
-        <Link href="/roadmap">
-          <a className={router.pathname == "/roadmap" ? "koro text-indigo-600" : ""}>ROADMAP</a>
-        </Link>
-      </div>
-      <div className="p-4">
-        <Link href="/whitepaper">
-          <a className={router.pathname == "/whitepaper" ? "koro text-indigo-600" : ""}>ABOUT</a>
-        </Link>
-      </div>
+      {Object.keys(routes).map((route, i) => (
+        <div className="p-4" key={`route-${i}`}>
+          <Link href={route}>
+            <a className={cls(baseClasses, router.pathname == route ? "koro uppercase text-indigo-600" : "")}>
+              routes[route]
+            </a>
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
