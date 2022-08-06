adb5563a9bada9fbeb7aa2efdfa6eec145e5e136
Rashawn Evans <Reva8390@gmail.com>
2022-08-06 10:00:28 -0400

    updates

--- pages/api/spotify.ts
+++ pages/api/spotify.ts
@@ -1,19 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-var SpotifyWebApi = require('spotify-web-api-node');
+var SpotifyWebApi = require("spotify-web-api-node")
 
 // credentials are optional
 var spotifyApi = new SpotifyWebApi({
-//   clientId: process.env.SPOTIFY_CLIENT_ID,
-//   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-//   redirectUri: process.env.SPOTIFY_CALLBACK_URL
-        
-});
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.SPOTIFY_CALLBACK_URL,
+})
 
-const authorizationCode =
-  '<insert authorization code with user-read-private and user-read-email scopes>';
+// Retrieve an access token and a refresh token
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  },
+  function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+  }
+)
 
-  // When our access token will expire
-let tokenExpirationEpoch;
+const authorizationCode = "<insert authorization code with user-read-private and user-read-email scopes>"
+
+// When our access token will expire
+let tokenExpirationEpoch
 
 // First retrieve an access token
 // spotifyApi.authorizationCodeGrant(authorizationCode).then(
@@ -21,7 +32,7 @@ let tokenExpirationEpoch;
 //       // Set the access token and refresh token
 //       spotifyApi.setAccessToken(data.body['access_token']);
 //       spotifyApi.setRefreshToken(data.body['refresh_token']);
-  
+
 //       // Save the amount of seconds until the access token expired
 //       tokenExpirationEpoch =
 //         new Date().getTime() / 1000 + data.body['expires_in'];
@@ -70,7 +81,7 @@ let tokenExpirationEpoch;
 //   }
 // );
 
-console.log('spotifyApi ', spotifyApi)
+console.log("spotifyApi ", spotifyApi)
 
 // spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
 //     function(data) {
@@ -126,7 +137,6 @@ console.log('spotifyApi ', spotifyApi)
 //   console.log('Something went wrong!', err);
 // });
 
-export default spotifyApi;
+export default spotifyApi
 
 // export { LOGIN_URL };
-  
