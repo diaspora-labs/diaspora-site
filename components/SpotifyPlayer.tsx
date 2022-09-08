e14556f26f5ac01d18e1096b3d3675c7449eae2a
Pedro.Acosta <pedro.acosta613@gmail.com>
2022-09-08 16:47:22 -0400

    push embid

---
+++ components/SpotifyPlayer.tsx
@@ -0,0 +1,22 @@
+return(<div id="embed-iframe"></div>
+    <script src="https://open.spotify.com/embed-podcast/iframe-api/v1" async>
+    </script>
+    <script type="text/javascript">
+      window.onSpotifyIframeApiReady = (IFrameAPI) => {
+        let element = document.getElementById('embed-iframe');
+        let options = {
+            width: '60%',
+            height: '200',
+            uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+          };
+        let callback = (EmbedController) => {
+          document.querySelectorAll('ul#episodes > li > button').forEach(
+            episode => {
+              episode.addEventListener('click', () => {
+                EmbedController.loadUri(episode.dataset.spotifyId)
+              });
+            })
+        };
+        IFrameAPI.createController(element, options, callback);
+      };
+    </script>)
