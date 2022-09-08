e88d06d71f30c1db9285dff3ce3eb92344ff29f2
Pedro.Acosta <pedro.acosta613@gmail.com>
2022-09-08 16:49:59 -0400

    added spotify component

--- components/SpotifyPlayer.tsx
+++ components/SpotifyPlayer.tsx
@@ -1,22 +1,28 @@
-return(<div id="embed-iframe"></div>
-    <script src="https://open.spotify.com/embed-podcast/iframe-api/v1" async>
-    </script>
-    <script type="text/javascript">
-      window.onSpotifyIframeApiReady = (IFrameAPI) => {
-        let element = document.getElementById('embed-iframe');
-        let options = {
-            width: '60%',
-            height: '200',
-            uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
-          };
-        let callback = (EmbedController) => {
-          document.querySelectorAll('ul#episodes > li > button').forEach(
-            episode => {
-              episode.addEventListener('click', () => {
-                EmbedController.loadUri(episode.dataset.spotifyId)
-              });
-            })
+import React from "react";
+
+export const ReactPlayer = () => {
+    return(
+    <div id="embed-iframe"></div>
+        <script src="https://open.spotify.com/embed-podcast/iframe-api/v1" async>
+        </script>
+        <script type="text/javascript">
+        window.onSpotifyIframeApiReady = (IFrameAPI) => {
+            let element = document.getElementById('embed-iframe');
+            let options = {
+                width: '60%',
+                height: '200',
+                uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+            };
+            let callback = (EmbedController) => {
+            document.querySelectorAll('ul#episodes > li > button').forEach(
+                episode => {
+                episode.addEventListener('click', () => {
+                    EmbedController.loadUri(episode.dataset.spotifyId)
+                });
+                })
+            };
+            IFrameAPI.createController(element, options, callback);
         };
-        IFrameAPI.createController(element, options, callback);
-      };
-    </script>)
+        </script>
+    )
+}
