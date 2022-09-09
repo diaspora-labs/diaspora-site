f39f04d84fa79490e447656c9c696a7dc5da94d0
Pedro.Acosta <pedro.acosta613@gmail.com>
2022-09-09 16:16:24 -0400

    fix hydration issue

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ReactPlayer from "react-player"
 
 export const MusicPlayer = () => {
   const [play, setPlay] = useState(true)
+  const [hasWindow, setHasWindow] = useState(false);
   const playlist = "https://www.youtube.com/playlist?list=PLZVVq-xf_m0kjiDeDSm2sW2dML4-LlrU7"
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
   return (
     <div>
-      <ReactPlayer url={playlist} playing={play} height={0} width={1} />
+      {hasWindow && <ReactPlayer url={playlist} playing={play} height={100} width={100} />}
 
+      
       <button onClick={() => setPlay(!play)}>Play</button>
     </div>
   )
