4b70d397e9865a88f86922e5edf7c9ccb3a235d0
Luc Succes <l2succes@gmail.com>
2022-10-03 21:09:01 -0500

    Update chat icon

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from "react"
+import React, { useState } from "react"
 import Script from "next/script"
+
 declare var Landbot: any
 
 const Chatbot = () => {
+  const [landbot, setLandbot] = useState(null)
+
   function initLandbot() {
     var s = document.createElement("script")
     s.type = "text/javascript"
     s.async = true
     s.addEventListener("load", function () {
-      new Landbot.Popup({
+      const l = new Landbot.Popup({
         configUrl: "https://chats.landbot.io/v3/H-1228722-I7RHH311Y3TAH6H3/index.json",
+        launcher_image: null,
+        launcherOptions: {
+          enable: false,
+        },
       })
+      setLandbot(l)
     })
     s.src = "https://cdn.landbot.io/landbot-3/landbot-3.0.0.js"
     var x = document.getElementsByTagName("script")[0]
