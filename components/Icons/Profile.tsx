ba4ab8a776925ae9e30d696ad76039de7bfa74b9
Pedro.Acosta <pedro.acosta613@gmail.com>
2022-10-06 10:03:17 -0400

    updated the plus to the green color

--- components/Icons/Profile.tsx
+++ components/Icons/Profile.tsx
@@ -8,7 +8,7 @@ export function Profile(props: { color?: string }) {
       width="25.044"
       height="25.044"
       viewBox="0 0 25.044 25.044"
-      className="stroke-[#2d2a26] transition ease-in-out group-hover:stroke-purple-light"
+      className="stroke-[#2d2a26] transition ease-in-out group-hover:stroke-green-hover"
     >
       <line y2="24.044" transform="translate(12.022)" fill="none" strokeLinecap="round" strokeWidth="1" />
       <line
