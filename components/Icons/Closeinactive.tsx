917a4e5453f7130d79ed1b99b9cc91312c4c27e8
Pedro.Acosta <pedro.acosta613@gmail.com>
2022-10-06 11:25:16 -0400

    removed some stuff and added color to 'fill'

--- components/Icons/Closeinactive.tsx
+++ components/Icons/Closeinactive.tsx
@@ -7,18 +7,18 @@ export function Closeinactive(props: { color?: string }) {
       width="26.379"
       height="26.379"
       viewBox="0 0 26.379 26.379"
-      className="hover:fill-current stroke-[#000] text-green-hover transition ease-in-out group-hover:stroke-green-hover hover:text-green-hover">
+      className="stroke-[#000] transition ease-in-out group-hover:stroke-green-hover">
         <g id="Group_240" data-name="Group 240" transform="translate(-1047.776 -1648.776)">
           <line 
             y2="35.306" 
             transform="translate(1073.448 1649.483) rotate(45)" 
-            fill="none" 
+            fill={color} 
             strokeLinecap="round" 
             strokeWidth="1"/>
           <line 
             y2="35.306" 
             transform="translate(1073.448 1674.448) rotate(135)" 
-            fill="none" 
+            fill={color} 
             strokeLinecap="round" 
             strokeWidth="1"/>
         </g>
