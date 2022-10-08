2c26dc5e5164c46898b96e6d4629d40631f6c76a
Rashawn Evans <Reva8390@gmail.com>
2022-10-08 10:50:44 -0400

    add scroll animation

--- components/HomeVisual.tsx
+++ components/HomeVisual.tsx
@@ -7,13 +7,29 @@ import animation from "../data/DiasporaVisual.json"
 export const Visual = () => {
   // load animation
   useEffect(() => {
+    var animDuration = 2000
     const anim = lottie.loadAnimation({
       container: document.querySelector("#roadmap-animation") as HTMLElement,
       animationData: animation,
       renderer: "svg",
-      loop: true,
-      autoplay: true,
+      loop: false,
+      autoplay: false,
     })
+
+    function animatebodymovin(duration: number) {
+      const scrollPosition = window.scrollY
+      const maxFrames = anim.totalFrames
+      const frame = (maxFrames / 100) * (scrollPosition / (duration / 100))
+      if (frame <= maxFrames) {
+        anim.goToAndStop(frame, true)
+      }
+    }
+
+    const onScroll = () => {
+      animatebodymovin(animDuration)
+    }
+
+    document.addEventListener("scroll", onScroll)
 
     return () => {
       anim.destroy()
