70f683626d7f05fc0803c054b3ce943ce7218a7f
Pedro.Acosta <pedro.acosta613@gmail.com>
2022-10-08 11:32:01 -0400

    margin for logo animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,7 +88,7 @@ const Home: NextPage = () => {
       </span>
 
       <section className={"pointer-events-auto z-10 flex h-full flex-col border-t-[1px] border-neutral-800"}>
-        <div className="my-auto flex grow flex-col p-10">
+        <div className="my-auto mt-10 flex grow flex-col p-10">
           <div className="mx-auto">
             <Visual /> 
             {/* <Image src="/images/logos/diaspora-team-logo.png" alt="screenshot" width="652" height="172" /> */}
