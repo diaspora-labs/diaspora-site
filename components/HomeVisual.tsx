fb9e6f5a89f71b34fbf3331aceb5c09ab8ace2b1
Rashawn Evans <Reva8390@gmail.com>
2022-10-08 08:31:47 -0400

    remove scroll comments

--- components/HomeVisual.tsx
+++ components/HomeVisual.tsx
@@ -7,7 +7,6 @@ import animation from "../data/DiasporaVisual.json"
 export const Visual = () => {
   // load animation
   useEffect(() => {
-    var animDuration = 1000
     const anim = lottie.loadAnimation({
       container: document.querySelector(".roadmap-animation") as HTMLElement,
       animationData: animation,
@@ -16,24 +15,8 @@ export const Visual = () => {
       autoplay: true,
     })
 
-    // function animatebodymovin(duration: number) {
-    //   const scrollPosition = window.scrollY
-    //   const maxFrames = anim.totalFrames
-    //   const frame = (maxFrames / 100) * (scrollPosition / (duration / 100))
-    //   if (frame <= maxFrames) {
-    //     anim.goToAndStop(frame, true)
-    //   }
-    // }
-
-    // const onScroll = () => {
-    //   animatebodymovin(animDuration)
-    // }
-
-    // document.addEventListener("scroll", onScroll)
-
     return () => {
       anim.destroy()
-    //   document.removeEventListener("scroll", onScroll)
     }
   }, [])
 
