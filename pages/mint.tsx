4d294f317a6783dc3ecfac174de758a9a6d0392f
Luc Succes <l2succes@gmail.com>
2022-10-21 00:18:16 -0500

    Mobile friendly

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,5 +1,5 @@
 import { useWallet } from "@solana/wallet-adapter-react"
-import { useProgram, useClaimNFT } from "@thirdweb-dev/react/solana"
+// import { useProgram, useClaimNFT } from "@thirdweb-dev/react/solana"
 import { HomeFooter } from "../components/Home/HomeFooter"
 import { Layout } from "../components/Layout"
 import Image from "next/image"
@@ -7,63 +7,41 @@ import { Instagram } from "../components/Icons/Instagram"
 import { Twitter } from "../components/Icons/Twitter"
 import { Discord } from "../components/Icons/Discord"
 
+const ntfs = [
+  {
+    url: "/images/masks/nft-1.jpg",
+    name: "Cote D'Ivoire",
+    description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+    cost: 0.2,
+  },
+  {
+    url: "/images/masks/nft-2.jpg",
+    name: "DAN",
+    description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+    cost: 0.5,
+  },
+  {
+    url: "/images/masks/nft-3.jpg",
+    name: "Red Mbambi",
+    description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+    cost: 0.7,
+  },
+]
+
 const Mint = () => {
-  const ntfs = [
-    {
-      url: "/images/masks/nft-1.jpg",
-      name: "Cote D'Ivoire",
-      description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
-      cost: 0.2,
-    },
-    {
-      url: "/images/masks/nft-2.jpg",
-      name: "DAN",
-      description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
-      cost: 0.5,
-    },
-    {
-      url: "/images/masks/nft-3.jpg",
-      name: "Red Mbambi",
-      description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
-      cost: 0.7,
-    },
-  ]
-
-  const Mask = ({ url, name, cost, description }) => {
-    return (
-      <div className="mx-5 w-[250px] flex-col">
-        <div className="mb-2">
-          <Image width="250" height="250" src={url} />
-        </div>
-
-        <div className="text-center">
-          <div className="text-lg font-bold text-white">{name}</div>
-          <div className="text-md my-2 font-light text-neutral-400">{description}</div>
-          <div className="text-bold my-3 flex flex-row items-center justify-center">
-            <Image src="/images/logos/solana-sol-logo.png" width="20" height="20" />{" "}
-            <span className="ml-2">{cost}</span>
-          </div>
-          <div className="mt-1">
-            <button className="mt-2 rounded-lg bg-purple-med px-4 py-2 text-white">Mint Membership</button>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  const wallet = useWallet()
 
   return (
     <Layout>
       <div className="absolute inset-0 z-0 w-full bg-roadmap-bg bg-cover"></div>
 
-      <div className="mb-30 mx-auto mt-40 max-w-4xl">
+      <div className="mb-30 mx-auto mt-40 lg:max-w-4xl ">
         <div className="my-10 flex flex-col">
-          <div className="mx-auto max-w-3xl">
-            <p className="py-8 text-3xl tracking-wide text-white md:text-4xl lg:pt-24">
-              Introducing "Masks" NFT pre-mint
-              <br />
-              for Diaspora DAO.
+          <div className="mx-5 lg:mx-auto lg:w-full lg:max-w-3xl">
+            <p className="py-8 text-2xl tracking-wide text-white lg:pt-24 lg:text-4xl">
+              Introducing "Masks" NFT pre-mint for Diaspora DAO.
             </p>
-            <p className="my-4 mr-20 text-lg font-light text-neutral-400">
+            <p className="my-4 text-lg font-light text-neutral-400 lg:mr-20">
               Experience unique rare NFTs that will give a sneak peek to the Diaspora DAO NFT collection. Masks have
               deep cultural history across the world, which includes access to the metaphysical. The “Masks” collection
               will unlock many benefits to users that purchase the NFT.
@@ -89,7 +67,7 @@ const Mint = () => {
             </div>
           </div>
 
-          <div className="mx-auto mt-10 max-w-3xl">
+          <div className="mx-5 mt-10 max-w-3xl lg:mx-auto">
             <h3 className="mb-10 text-2xl">Why Diaspora DAO</h3>
 
             <div className="mt-2 font-light text-neutral-400">
@@ -111,7 +89,7 @@ const Mint = () => {
             </div>
           </div>
 
-          <div className="mt-10 mb-20  rounded-lg border border-neutral-700 p-10 text-center">
+          <div className="mx-5 mt-10 mb-20  rounded-lg border border-neutral-700 p-10 text-center">
             <h3 className="mb-10 text-2xl">Stay connected.</h3>
             <div className="flex flex-row justify-center">
               <span className=" flex flex-row items-center space-x-20">
@@ -137,6 +115,27 @@ const Mint = () => {
 
       <HomeFooter />
     </Layout>
+  )
+}
+
+const Mask = ({ url, name, cost, description }) => {
+  return (
+    <div className="mx-auto mb-10 w-full flex-col items-center justify-center text-center lg:mx-5 lg:w-[250px]">
+      <div className="mb-2">
+        <Image width="250" height="250" src={url} />
+      </div>
+
+      <div className="text-center">
+        <div className="text-lg font-bold text-white">{name}</div>
+        <div className="text-md my-2 font-light text-neutral-400">{description}</div>
+        <div className="text-bold my-3 flex flex-row items-center justify-center">
+          <Image src="/images/logos/solana-sol-logo.png" width="20" height="20" /> <span className="ml-2">{cost}</span>
+        </div>
+        <div className="mt-1">
+          <button className="mt-2 rounded-lg bg-purple-med px-4 py-2 text-white">Mint Membership</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
