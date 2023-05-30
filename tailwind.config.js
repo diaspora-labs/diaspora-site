b188cf2470d7d52be703890069f0a1561e308bd0
anushady <anushadycash@gmail.com>
2023-05-30 18:47:56 +0300

    update new home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,23 @@ module.exports = {
           med: "#98989A"
         }
       },
+      screens: {
+        "max-2xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        "max-xl": { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        "max-lg": { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        "max-md": { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        "max-sm": { max: "639px" },
+        "max-smm": { max: "430px" },
+        // => @media (max-width: 639px) { ... }
+      },
       transitionProperty: {
         height: "height",
       },
