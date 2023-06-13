ee56a622e21d8dc8f635b9f26a20e117aaba43a5
PledgeNexus <Reva8390@gmail.com>
2023-06-13 17:24:53 -0400

    revert

--- pages/player.tsx
+++ pages/player.tsx
@@ -1,42 +1,6 @@
-import React, { useEffect } from "react"
 import { Layout } from "../components/Layout"
 
 const Player = () => {
-  useEffect(() => {
-    // window.addEventListener("message", handleMessage)
-    // window.parent.postMessage(
-    //   {
-    //     type: "itemKey",
-    //     data: {
-    //       fun1: () => {
-    //         console.log("Testing")
-    //       },
-    //     },
-    //   },
-    //   "https://journey-taupe.vercel.app"
-    // )
-    const iframe = document.getElementById("my-iframe")
-    iframe.contentWindow.postMessage(
-      {
-        type: "itemKey",
-        data: {
-          fun1: () => {
-            console.log("Testing")
-          },
-        },
-      },
-      "https://journey-taupe.vercel.app"
-    )
-  }, [])
-
-  function handleMessage(event) {
-    if (event.data.type === "itemKey") {
-      console.log("Received message data from iFrame: ", event.data.data)
-      // Here is where we call our NFT FUNCTION
-      // someFunction(event.data.data)
-    }
-  }
-
   return (
     <Layout>
       <div className="relative h-screen flex flex-col justify-center items-center max-2xl:h-[90vh] max-xl:h-[75vh] max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[35vh] max-smm-[18vh]">
