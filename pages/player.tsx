972f51086aaa3f859454c95539b0de3705754a05
Pedro Acosta <pedro.acosta613@gmail.com>
2023-06-13 17:08:45 -0400

    added some styling

--- pages/player.tsx
+++ pages/player.tsx
@@ -3,9 +3,9 @@ import { Layout } from "../components/Layout"
 const Player = () => {
   return (
     <Layout>
-      <div className="w-screen h-screen flex items-center justify-center">
-        <iframe className="w-full h-full" id="my-iframe" src="https://journey-taupe.vercel.app"></iframe>
-        </div>
+      <section className="relative h-screen flex flex-col justify-center items-center max-2xl:h-[90vh] max-xl:h-[75vh] max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[35vh] max-smm-[18vh]">
+        <iframe className="w-full h-5/6" id="my-iframe" src="https://journey-taupe.vercel.app"></iframe>
+      </section>
     </Layout>
   )
 }
