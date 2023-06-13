c629d4a10032e367ee4475c205e1d887e74148e8
Pedro Acosta <pedro.acosta613@gmail.com>
2023-06-13 16:59:05 -0400

    made screen bigger

--- pages/player.tsx
+++ pages/player.tsx
@@ -3,7 +3,9 @@ import { Layout } from "../components/Layout"
 const Player = () => {
   return (
     <Layout>
+      <div className="w-screen h-screen flex items-center justify-center">
         <iframe className="w-full h-full" id="my-iframe" src="https://journey-taupe.vercel.app"></iframe>
+        </div>
     </Layout>
   )
 }
