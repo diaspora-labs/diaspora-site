f6c205f94fa6cd116847a245909bcf5589cf336f
Pedro Acosta <pedro.acosta613@gmail.com>
2023-06-13 17:19:08 -0400

    change back to div

--- pages/player.tsx
+++ pages/player.tsx
@@ -3,9 +3,9 @@ import { Layout } from "../components/Layout"
 const Player = () => {
   return (
     <Layout>
-      <section className="relative h-screen flex flex-col justify-center items-center max-2xl:h-[90vh] max-xl:h-[75vh] max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[35vh] max-smm-[18vh]">
+      <div className="relative h-screen flex flex-col justify-center items-center max-2xl:h-[90vh] max-xl:h-[75vh] max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[35vh] max-smm-[18vh]">
         <iframe className="w-full h-5/6" id="my-iframe" src="https://journey-taupe.vercel.app"></iframe>
-      </section>
+      </div>
     </Layout>
   )
 }
