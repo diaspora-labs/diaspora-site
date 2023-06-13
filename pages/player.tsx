45193db2dd8b2e165d4ee9d720d89ca0d079a68a
PledgeNexus <Reva8390@gmail.com>
2023-06-13 17:26:59 -0400

    current

--- pages/player.tsx
+++ pages/player.tsx
@@ -1,10 +1,46 @@
+import React, { useEffect } from "react"
 import { Layout } from "../components/Layout"
 
 const Player = () => {
+  useEffect(() => {
+    // window.addEventListener("message", handleMessage)
+    // window.parent.postMessage(
+    //   {
+    //     type: "itemKey",
+    //     data: {
+    //       fun1: () => {
+    //         console.log("Testing")
+    //       },
+    //     },
+    //   },
+    //   "https://journey-taupe.vercel.app"
+    // )
+    const iframe = document.getElementById("my-iframe")
+    // iframe.contentWindow.postMessage(
+    //   {
+    //     type: "itemKey",
+    //     data: {
+    //       fun1: () => {
+    //         console.log("Testing")
+    //       },
+    //     },
+    //   },
+    //   "https://journey-taupe.vercel.app"
+    // )
+  }, [])
+
+  function handleMessage(event) {
+    if (event.data.type === "itemKey") {
+      console.log("Received message data from iFrame: ", event.data.data)
+      // Here is where we call our NFT FUNCTION
+      // someFunction(event.data.data)
+    }
+  }
+
   return (
     <Layout>
-      <div className="relative h-screen flex flex-col justify-center items-center max-2xl:h-[90vh] max-xl:h-[75vh] max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[35vh] max-smm-[18vh]">
-        <iframe className="w-full h-5/6" id="my-iframe" src="https://journey-taupe.vercel.app"></iframe>
+      <div className="max-smm-[18vh] relative flex h-screen flex-col items-center justify-center max-2xl:h-[90vh] max-xl:h-[75vh] max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[35vh]">
+        <iframe className="h-5/6 w-full" id="my-iframe" src="https://journey-taupe.vercel.app"></iframe>
       </div>
     </Layout>
   )
