820f3e04ccfabef27eae2e2aee899ff0ccff5c96
Luc Succes <l2succes@gmail.com>
2023-06-15 11:08:23 -0600

    Hide music player on player

--- pages/player.tsx
+++ pages/player.tsx
@@ -4,28 +4,22 @@ import { Layout } from "../components/Layout"
 const Player = () => {
   useEffect(() => {
     window.addEventListener("message", handleMessage)
-    window.parent.postMessage(
-      {
-        type: "itemKey",
-        data: { hello: "world" },
-      },
-      "*"
-      // "https://journey-taupe.vercel.app"
-    )
+    window.document.addEventListener("Journey:PlayerRendered", handleMessage)
   }, [])
 
   const handleMessage = (event) => {
-    if (event.data.type === "itemKey") {
-      console.log("Received message data from iFrame: ", event.data.data)
-      // Here is where we call our NFT FUNCTION
-      // someFunction(event.data.data)
-    }
+    console.log("Event", event)
+    // if (event.data.type === "itemKey") {
+    //   console.log("Received message data from iFrame: ", event.data.data)
+    //   // Here is where we call our NFT FUNCTION
+    //   // someFunction(event.data.data)
+    // }
   }
 
   return (
     <Layout>
       <div className="max-smm-[18vh] relative flex h-screen flex-col items-center justify-center max-2xl:h-[90vh] max-xl:h-[75vh] max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[35vh]">
-        <iframe className="h-5/6 w-full" id="my-iframe" src="https://journey-taupe.vercel.app"></iframe>
+        <iframe className="h-5/6 w-full" id="my-iframe" src="http://localhost:19006/"></iframe>
       </div>
     </Layout>
   )
