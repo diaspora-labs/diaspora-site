cf8704181a0a8b65f20d7a5c190d1db48a12702b
Luc Succes <l2succes@gmail.com>
2023-06-19 17:51:13 -0600

    Clean up mint

--- pages/player.tsx
+++ pages/player.tsx
@@ -1,25 +1,34 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { Layout } from "../components/Layout"
+import { NFTModal } from "@/components/NFTModal"
 
 const Player = () => {
+  const [editionNft, setEditionNft] = useState(undefined)
+  const [nft, setNft] = useState(undefined)
+  const [showModal, setShowModal] = useState(false)
+
   useEffect(() => {
     window.addEventListener("message", handleMessage)
     window.document.addEventListener("Journey:PlayerRendered", handleMessage)
   }, [])
 
   const handleMessage = (event) => {
-    console.log("Event", event)
-    // if (event.data.type === "itemKey") {
-    //   console.log("Received message data from iFrame: ", event.data.data)
-    //   // Here is where we call our NFT FUNCTION
-    //   // someFunction(event.data.data)
-    // }
+    const eventName = event.data.event
+    if (eventName && eventName.includes("Journey:")) {
+      console.log("Event", event)
+      if (eventName === "Journey:NftSelected") {
+        setNft(event.data.data.nft)
+        setShowModal(true)
+      }
+    }
   }
 
   return (
     <Layout>
       <div className="max-smm-[18vh] relative flex h-screen flex-col items-center justify-center max-2xl:h-[90vh] max-xl:h-[75vh] max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[35vh]">
         <iframe className="h-5/6 w-full" id="my-iframe" src="http://localhost:19006/"></iframe>
+
+        {showModal && nft && <NFTModal nft={nft} setModal={setShowModal} />}
       </div>
     </Layout>
   )
