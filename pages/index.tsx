b3b5331ac1e24fda72bb967e5242f20b5441ca4b
Pedro Acosta <pedro.acosta613@gmail.com>
2023-11-22 10:31:36 -0500

    its looking much better

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,45 +26,81 @@ const Home: NextPage = () => {
   return (
     <Layout showLogo>
       <div className="relative bg-home-bg-labs bg-cover min-h-screen flex flex-col items-center justify-center text-white">
-      <div className="absolute inset-0 bg-gradient-to-r mix-blend-overlay from-custom-black to-custom-purple opacity-80"></div>
+        <div className="absolute inset-0 bg-gradient-to-r mix-blend-overlay from-custom-black to-custom-purple opacity-80"></div>
         
-        <div className="h-[462px] left-[258px] top-[203px] absolute flex-col justify-start items-start gap-[60px] inline-flex">
-            <div className="h-[205px] flex-col justify-start items-start gap-7 flex">
-                <div className="self-stretch h-[78px]"><span className="text-white text-[102px] font-black font-['Korolev']">D</span><span className="text-white text-[102px] font-black font-['Korolev']">I</span><span className="text-white text-[102px] font-black font-['Korolev']">A</span><span className="text-white text-[102px] font-black font-['Korolev']">S</span><span className="text-white text-[102px] font-black font-['Korolev']">P</span><span className="text-white text-[102px] font-black font-['Korolev']">O</span><span className="text-white text-[102px] font-black font-['Korolev']">R</span><span className="text-white text-[102px] font-black font-['Korolev']">A</span><span className="text-white text-[102px] font-black font-['Korolev']"> </span><span className="text-white text-[102px] font-black font-['Korolev']">L</span><span className="text-white text-[102px] font-black font-['Korolev']">AB</span><span className="text-white text-[102px] font-black font-['Korolev']">S</span></div>
-                <div className="self-stretch text-white text-[22px] font-light font-['Korolev'] leading-[33px]">Diaspora is a DAO and Research Lab leveraging AI, Web 3, and Mixed Reality technology for the betterment of underrepresented communities in both the physical and digital worlds.</div>
+        <div className="flex flex-col w-[852px] items-start gap-[60px] relative">
+          <div className="flex flex-col w-[692px] items-start gap-[28px] relative flex-[0_0_auto]">
+            <p className="relative self-stretch h-[78px] mt-[-1.00px] [font-family:'Korolev-Regular',Helvetica] font-normal text-white text-[102px] leading-[normal] whitespace-nowrap">
+              <span className="tracking-[-10.20px]">D</span>
+              <span className="tracking-[-7.14px]">I</span>
+              <span className="tracking-[-10.20px]">A</span>
+              <span className="tracking-[-9.18px]">S</span>
+              <span className="tracking-[-8.16px]">P</span>
+              <span className="tracking-[-10.20px]">O</span>
+              <span className="tracking-[-7.14px]">R</span>
+              <span className="tracking-[-10.20px]">A</span>
+              <span className="tracking-[0]">&nbsp;</span>
+              <span className="tracking-[-1.02px]">L</span>
+              <span className="tracking-[-6.12px]">AB</span>
+              <span className="tracking-[-10.20px]">S</span>
+            </p>
+            <p className="relative self-stretch [font-family:'Korolev-Light',Helvetica] font-light text-white text-[22px] tracking-[0] leading-[33px]">
+              Diaspora is a DAO and Research Lab leveraging AI, Web 3, and Mixed Reality technology for the betterment of
+              underrepresented communities in both the physical and digital worlds.
+            </p>
+          </div>
+          <div className="flex flex-col w-[852px] items-start gap-[26px] relative flex-[0_0_auto]">
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[22px] tracking-[0] leading-[33px] whitespace-nowrap">
+              Specializing in:
             </div>
-            <div className="h-[197px] flex-col justify-start items-start gap-[26px] flex">
-                <div className="text-white text-[22px] font-light font-['Korolev'] leading-[33px]">Specializing in:</div>
-                <div className="w-[852px] justify-start items-start gap-5 inline-flex">
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">Blockchain</div>
-                    </div>
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">Smart Contracts</div>
-                    </div>
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">Distributed Ledger Technology</div>
-                    </div>
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">VR</div>
-                    </div>
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">Mixed Reality</div>
-                    </div>
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">DEFI</div>
-                    </div>
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">Software Development</div>
-                    </div>
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">DAO</div>
-                    </div>
-                    <div className="px-6 py-4 rounded-xl border border-violet-600 justify-center items-center gap-2.5 flex">
-                        <div className="text-white text-lg font-light font-['Korolev'] leading-[27px] tracking-wide">AR</div>
-                    </div>
+            <div className="flex flex-wrap w-[852px] items-start gap-[20px_20px] relative flex-[0_0_auto]">
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  Blockchain
                 </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  Distributed Ledger Technology
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  Software Development
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  AR
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  Smart Contracts
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  VR
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  Mixed Reality
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  DEFI
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[24px] py-[16px] rounded-[12px] border border-solid border-[#7233ff] relative flex-[0_0_auto]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Korolev-Light',Helvetica] font-light text-white text-[18px] tracking-[0.72px] leading-[27px] whitespace-nowrap">
+                  DAO
+                </div>
+              </div>
             </div>
+          </div>
         </div>
       </div>
       {/* <div className="opacity-80 w-[1579.51px] h-[985px] relative home-bg-labs">
